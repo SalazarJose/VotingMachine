64abb85c446ae937868f2310b060ac1f33ea4ff1
SalazarJose <RealJoseSalazar@gmail.com>
2017-01-22 21:59:00 -0600

    Modified file, but is currently not included in the running program.

--- c++/linkedlist.cpp
+++ c++/linkedlist.cpp
@@ -7,3 +7,45 @@
 //
 
 #include "linkedlist.hpp"
+
+//linkedlist.cpp
+
+Voter_List::Voter_List()
+{
+    front = NULL;
+    back = NULL;
+}
+Voter_List::~Voter_List()
+{
+    destroyVoterList();
+}
+
+
+void Voter_List:: appendVoterNode(int voterID, string voterName, bool voted)
+{
+    VOTER_NODE* n = new VOTER_NODE();
+    if(back == NULL)
+    {
+        back = n;
+        front = n;
+    }
+    else
+    {
+        back->next = n;
+        n-> previous = back;
+        back =n;
+    }
+}
+
+void Voter_List::displayVoters()
+{
+    cout << "Voters: " <<endl;
+    VOTER_NODE* temp = front;
+    
+    while(temp!= NULL)
+    {
+        cout << " " << temp->voterID << endl;
+        temp = temp->next;
+    }
+
+}
