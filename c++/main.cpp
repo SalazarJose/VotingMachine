a78e55e4a7319b00de79d47a0e8c38cce1593114
SalazarJose <RealJoseSalazar@gmail.com>
2017-01-22 21:56:15 -0600

    Updated the main.cpp file to work with the DEMO and linked list

--- c++/main.cpp
+++ c++/main.cpp
@@ -9,29 +9,45 @@
 #include "main.hpp"
 #include "machine.cpp"
 #include "resp.cpp"
-
+#include "linkedlist.hpp"
 int main(){
-
-    voter voter1;
     
     
-    cout << "Generating Test Voter.." <<endl;
-    /*Generate Voter Test*/
+    //MAIN PROGRAM IMPLEMENTATION
+    //This section will run the linked list that us still under construction.
+    
+    
+    cout << "*****This part is still under construction*****" <<endl;
+    voters myvoter1;
+    myvoter1.addvoter();
+    myvoter1.printList();
+    cout << "*****end of this section*****" <<endl;
+    //END OF LINKED LIST SECTION.
+    
+    
+    //DEMO BELOW*****
+    //This section will run a simple "demo", might be used for the above section later on.
+    voterDEMO voter1;
+    
+    cout << "*****This is a sample DEMO version.*****" <<endl;
+    //Generate Voter Test
     voter1.ModName("Robot # 1");
     voter1.ModID(1);
+    cout << "Running Auto-population" <<endl;
     voter1.ModExp();
+    cout << "Thanks!" <<endl;
     voter1.ModHasVoted(true);
-    
+
     
     cout << endl << endl <<endl;
     cout << "Pulling Data on Test Voter.." <<endl;
-    /*Return Voter Test Results*/
+    //Return Voter Test Results
     voter1.Pull_Name();
     voter1.Pull_ID();
     voter1.Pull_Exp();
     voter1.Pull_HasVoted();
     
-    /*End Program with Code 0*/
+    //End Program with Code 0 *****
     
     return 0;
 }
