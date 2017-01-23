7c5a80b3ed88efa9375448e2120745f953f9785d
SalazarJose <RealJoseSalazar@gmail.com>
2017-01-22 21:51:58 -0600

    Modified DEMO version, and started a linked list for voter and voters.

---
+++ c++/linkedlist.cpp
@@ -0,0 +1,9 @@
+//
+//  linkedlist.cpp
+//  
+//
+//  Created by Jose Salazar on 1/22/17.
+//
+//
+
+#include "linkedlist.hpp"
---
+++ c++/linkedlist.hpp
@@ -0,0 +1,14 @@
+//
+//  linkedlist.hpp
+//  
+//
+//  Created by Jose Salazar on 1/22/17.
+//
+//
+
+#ifndef linkedlist_hpp
+#define linkedlist_hpp
+
+#include <stdio.h>
+
+#endif /* linkedlist_hpp */
